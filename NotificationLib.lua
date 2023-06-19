b14c27047e70d7583dc663f3181001a3d29746e0
ayayy <101590205+ayayy@users.noreply.github.com>
2023-06-19 11:39:50 +0200

    Update NotificationLib.lua

--- NotificationLib.lua
+++ NotificationLib.lua
@@ -9,44 +9,41 @@ function NotificationLib:Notify(title, text, duration, notificationType)
     end
     wait()
     local Frame = Instance.new("Frame")
-    local Frame_2 = Instance.new("Frame")
     local UICorner = Instance.new("UICorner")
     local Title = Instance.new("TextLabel")
     local Text = Instance.new("TextLabel")
     local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
     local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
     local Type = Instance.new("ImageButton")
+    local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
+    local UIAspectRatioConstraint_3 = Instance.new("UIAspectRatioConstraint")
     local Text_2 = Instance.new("TextLabel")
     local UITextSizeConstraint_2 = Instance.new("UITextSizeConstraint")
+    local UIAspectRatioConstraint_4 = Instance.new("UIAspectRatioConstraint")
     local Done = Instance.new("ImageButton")
+    local UIAspectRatioConstraint_5 = Instance.new("UIAspectRatioConstraint")
     local ProgressBar = Instance.new("Frame")
     local Progress = Instance.new("Frame")
+    local UIAspectRatioConstraint_6 = Instance.new("UIAspectRatioConstraint")
+    local UIAspectRatioConstraint_7 = Instance.new("UIAspectRatioConstraint")
 
 
     ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
     ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
     Frame.Parent = ScreenGui
-    Frame.AnchorPoint = Vector2.new(0.5, 0.5)
-    Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Frame.BackgroundTransparency = 1.000
-    Frame.BorderSizePixel = 0
-    Frame.Position = UDim2.new(0, 2300, 0, 950)
-
-    Frame_2.Parent = Frame
-    Frame_2.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-    Frame_2.Position = UDim2.new(0, -303, 0, -79)
-    Frame_2.Size = UDim2.new(0, 392, 0, 124)
+    Frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+    Frame.Position = UDim2.new(2, -398, 1, -128)
+    Frame.Size = UDim2.new(0, 392, 0, 124)
 
     UICorner.CornerRadius = UDim.new(0, 6)
-    UICorner.Parent = Frame_2
+    UICorner.Parent = Frame
 
     Title.Name = "Title"
-    Title.Parent = Frame_2
+    Title.Parent = Frame
     Title.BackgroundColor3 = Color3.fromRGB(200, 200, 200)
     Title.BackgroundTransparency = 0.900
     Title.BorderSizePixel = 0
-    Title.Position = UDim2.new(-0.00255102036, 0, 0, 0)
     Title.Size = UDim2.new(0, 393, 0, 18)
     Title.Font = Enum.Font.SourceSans
     Title.Text = ""
@@ -58,7 +55,7 @@ function NotificationLib:Notify(title, text, duration, notificationType)
     Text.Parent = Title
     Text.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Text.BackgroundTransparency = 1.000
-    Text.Position = UDim2.new(0.0127226459, 0, 0.111111112, 0)
+    Text.Position = UDim2.new(0, 4, 0, 2)
     Text.Size = UDim2.new(0, 388, 0, 14)
     Text.Font = Enum.Font.GothamBold
     Text.Text = "Notification"
@@ -78,16 +75,21 @@ function NotificationLib:Notify(title, text, duration, notificationType)
     Type.Parent = Title
     Type.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Type.BackgroundTransparency = 1.000
-    Type.Position = UDim2.new(0.951498032, 0, 0.036738079, 0)
+    Type.Position = UDim2.new(0, 373, 0, 0)
     Type.Size = UDim2.new(0, 16, 0, 16)
     Type.Image = "rbxassetid://13012250972"
 
+    UIAspectRatioConstraint_2.Parent = Type
+
+    UIAspectRatioConstraint_3.Parent = Title
+    UIAspectRatioConstraint_3.AspectRatio = 21.833
+
     Text_2.Name = "Text"
-    Text_2.Parent = Frame_2
+    Text_2.Parent = Frame
     Text_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Text_2.BackgroundTransparency = 1.000
-    Text_2.Position = UDim2.new(0.0153061226, 0, 0.193548381, 0)
-    Text_2.Size = UDim2.new(0.974489808, 0, 0.604838729, 0)
+    Text_2.Position = UDim2.new(0, 6, 0, 23)
+    Text_2.Size = UDim2.new(0, 382, 0, 75)
     Text_2.Font = Enum.Font.SourceSans
     Text_2.Text = "This is a notification."
     Text_2.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -100,19 +102,24 @@ function NotificationLib:Notify(title, text, duration, notificationType)
     UITextSizeConstraint_2.Parent = Text_2
     UITextSizeConstraint_2.MaxTextSize = 16
 
+    UIAspectRatioConstraint_4.Parent = Text_2
+    UIAspectRatioConstraint_4.AspectRatio = 5.093
+
     Done.Name = "Done"
-    Done.Parent = Frame_2
+    Done.Parent = Frame
     Done.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Done.BackgroundTransparency = 1.000
-    Done.Position = UDim2.new(0.938775539, 0, 0.814516127, 0)
+    Done.Position = UDim2.new(0, 368, 0, 100)
     Done.Size = UDim2.new(0, 20, 0, 20)
     Done.Image = "rbxassetid://1202200114"
 
+    UIAspectRatioConstraint_5.Parent = Done
+
     ProgressBar.Name = "ProgressBar"
-    ProgressBar.Parent = Frame_2
+    ProgressBar.Parent = Frame
     ProgressBar.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
     ProgressBar.BorderSizePixel = 0
-    ProgressBar.Position = UDim2.new(0.0102040814, 0, 0.895161271, 0)
+    ProgressBar.Position = UDim2.new(0, 3, 0, 110)
     ProgressBar.Size = UDim2.new(0, 356, 0, 5)
 
     Progress.Name = "Progress"
@@ -121,34 +128,40 @@ function NotificationLib:Notify(title, text, duration, notificationType)
     Progress.BorderSizePixel = 0
     Progress.Size = UDim2.new(0.00280898879, 0, 1, 0)
 
+    UIAspectRatioConstraint_6.Parent = ProgressBar
+    UIAspectRatioConstraint_6.AspectRatio = 71.200
+
+    UIAspectRatioConstraint_7.Parent = Frame
+    UIAspectRatioConstraint_7.AspectRatio = 3.161
+
     --
     
     Text.Text = tostring(title) or "Notification"
     Text_2.Text = tostring(text) or "This is a notification."
-    Frame:TweenPosition(UDim2.new(0.95, 0, 0.95, 0), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
+    Frame:TweenPosition(UDim2.new(1, -398, 1, -128), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
     if count then
         for i,v in pairs(ScreenGui:GetChildren()) do
             if v.Name == "Frame" then
-                v:TweenPosition(v.Position - UDim2.new(0, 0, 0.132, 0), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
+                v:TweenPosition(v.Position - UDim2.new(0, 0, 0, 128), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
             end
         end
     end
     Done.MouseButton1Click:Connect(function()
-        Frame:TweenPosition(UDim2.new(1.1, 0, 0.949999988, 0), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
+        Frame:TweenPosition(UDim2.new(2, -398, 1, -128), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
         wait(0.2)
         Frame:Destroy()
         if count then
             for i,v in pairs(ScreenGui:GetChildren()) do
                 if v.Name == "Frame" then
-                    if tonumber(v.Position.Y.Offset) < tonumber(Frame_2.Position.Y.Offset) then
-                        v:TweenPosition(v.Position + UDim2.new(0, 0, 0.132, 0), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
+                    if tonumber(v.Position.Y.Offset) < tonumber(Frame.Position.Y.Offset) then
+                        v:TweenPosition(v.Position + UDim2.new(0, 0, 0, 128), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
                     end
                 end
             end
         end
     end)
     Progress:TweenSize(UDim2.new(1, 0, 1, 0), Enum.EasingDirection.In, Enum.EasingStyle.Linear, tonumber(duration) or 4, false, function()
-        Frame:TweenPosition(UDim2.new(1.1, 0, 0.949999988, 0), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
+        Frame:TweenPosition(UDim2.new(2, -398, 1, -128), Enum.EasingDirection.In, Enum.EasingStyle.Linear, 0.2)
         wait(0.2)
         Frame:Destroy()
     end)
