962fd144908754d26762d67300506aef231b568b
ayayy <101590205+ayayy@users.noreply.github.com>
2023-06-23 18:41:38 +0200

    Create Example.lua

---
+++ NotificationLib/Example.lua
@@ -0,0 +1,8 @@
+--created by theo
+
+--Loader:
+local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/ayayy/Libraries/main/NotificationLib/Source.lua"))()
+
+--Function:
+Loader:Notify("Test Notification", "This is a test notification.", 4, "Info")
+--Loader:Notify(<string> title, <string> text, <number> duration, <string> type ("Info", "Error", "Warning"))
