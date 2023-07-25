4a43fc679836ba1c85e0f3c92734db1d626a4e39
scrue <101590205+ayayy@users.noreply.github.com>
2023-07-25 13:11:29 +0200

    Update Source.lua

--- Byte/Source.lua
+++ Byte/Source.lua
@@ -137,7 +137,7 @@ function Byte:CreateWindow(name)
 
     local lastMousePos
     local lastGoalPos
-    local DRAG_SPEED = (8); -- // The speed of the UI darg.
+    local DRAG_SPEED = (12); -- // The speed of the UI darg.
     function Update(dt)
         if not (startPos) then return end;
         if not (dragging) and (lastGoalPos) then
